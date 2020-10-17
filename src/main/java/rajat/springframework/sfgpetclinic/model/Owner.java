d9f91072b0e6c4715bf378f64cadc88c3b5204d7
Rajat <rajat.0p9@gmail.com>
2020-10-17 15:18:53 -0500

    adding POJOs. Closes #2.

---
+++ src/main/java/rajat/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package rajat.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/rajat/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package rajat.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
