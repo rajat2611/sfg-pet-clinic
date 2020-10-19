158f4dcd82f6b0eb390aba4903274717a09db199
Rajat <rajat.0p9@gmail.com>
2020-10-19 12:18:41 -0500

    Implement Interfaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/rajat/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package rajat.springframework.sfgpetclinic.services;
+
+import rajat.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastname);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/rajat/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package rajat.springframework.sfgpetclinic.services;
+
+import rajat.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
