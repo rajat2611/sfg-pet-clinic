760ab792d8128cb9e0e09981dec6f8d35f7f59b8
Rajat <rajat.0p9@gmail.com>
2020-10-21 13:42:21 -0500

    Added vets list and updated controller. closes #15

--- pet-clinic-web/src/main/java/rajat/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/rajat/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package rajat.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rajat.springframework.sfgpetclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
